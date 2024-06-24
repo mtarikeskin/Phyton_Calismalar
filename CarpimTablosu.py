4a44854291074575ae5459465f6ddc335c67ce67
Tarık <m.keskin.tarik@gmail.com>
2024-06-24 18:27:31 +0300

    Add files via upload

---
+++ CarpimTablosu.py
@@ -0,0 +1,40 @@
+
+
+liste = list(range(1,11))
+
+for i in liste :
+    for j in liste :
+        sonuc = i * j
+        print("{} x {} = {}".format(i,j,sonuc))
+    print("*********************")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
