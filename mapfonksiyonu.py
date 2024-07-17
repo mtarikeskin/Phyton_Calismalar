8456f2ec2d2f4a75310e9a6ec1396256c94bac78
Tarık <m.keskin.tarik@gmail.com>
2024-07-17 14:06:54 +0300

    Add files via upload

---
+++ mapfonksiyonu.py
@@ -0,0 +1,26 @@
+
+
+def alan_hesapla(veri) :
+    x,y = veri
+    return x*y
+
+veriler = [(3,4), (10,3), (5,6), (1,9)]
+
+liste = list(map(alan_hesapla,veriler))
+print(liste)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
